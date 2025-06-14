26f6f3c8932dc985ff22ab568a98385503be8069
sadikozaydin <sadik@duendedijital.com>
2025-06-14 18:30:49 +0300

    dosyalar güncellendi

---
+++ database/migrations/2025_05_12_122157_alter_attendance_table_for_clockout_time_location.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+            $table->enum('clock_out_time_work_from_type', ['home', 'office', 'other'])->nullable();
+            $table->unsignedBigInteger('clock_out_time_location_id')->nullable()->index('attendances_clock_out_time_location_id_foreign');
+            $table->foreign(['clock_out_time_location_id'])->references(['id'])->on('company_addresses')->onUpdate('CASCADE')->onDelete('SET NULL');
+            $table->string('clock_out_time_working_from')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+            $table->dropColumn('clock_out_time_work_from_type');
+            $table->dropColumn('clock_out_time_location_id');
+            $table->dropColumn('clock_out_time_working_from');
+        });
+    }
+};
